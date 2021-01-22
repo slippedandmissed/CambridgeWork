e2b7d753407b3bacb2c6b81aac15216b83ea7413
Morgan Saville <gbt505@hotmail.com>
2021-01-22 16:25:50 +0000

    ML Tick 1

---
+++ Machine Learning/Task 1/src/main/module-info.java
@@ -0,0 +1,6 @@
+module task1 {
+	exports uk.ac.cam.cl.jbs52.exercises.sentiment_detection;
+	exports uk.ac.cam.cl.jbs52.testing;
+
+	requires stanford.postagger;
+}
---
+++ Machine Learning/Task 1/src/main/uk/ac/cam/cl/jbs52/exercises/sentiment_detection/Sentiment.java
@@ -0,0 +1,6 @@
+package uk.ac.cam.cl.jbs52.exercises.sentiment_detection;
+
+
+public enum Sentiment {
+	POSITIVE, NEGATIVE;
+}
