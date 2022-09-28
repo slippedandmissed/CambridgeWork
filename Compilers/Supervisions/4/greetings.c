3bbb5fab5cf65d64e442ac6be8113ca1c1cdef6b
slippedandmissed <gbt505@hotmail.com>
2022-09-28 07:15:17 +0100

    Fresh start

--- Compilers/Supervisions/4/greetings.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char *greetings(void) { return "Hello, world\n"; }
-int main()
-{
-	char *p = greetings();
-	p[0] = 'G';
-	puts(p);
-}
--- Prolog/Supervisions/Supervision 1/6.pl
+++
@@ -1,2 +0,0 @@
-append([],A,A).
-append([H|T],A,[H|R]) :- append(T,A,R).
