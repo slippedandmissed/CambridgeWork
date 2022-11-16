d7f37a33af75b0b7b5be76c0c9d0761d969f7914
slippedandmissed <gbt505@hotmail.com>
2022-11-16 22:22:45 +0000

    Rewrote discrete algorithms to be neater

--- Dissertation/notebooks/survey_of_discrete_algorithms/custom_types.py
+++ Dissertation/notebooks/survey_of_discrete_algorithms/custom_types.py
@@ -1,10 +1,10 @@
-from typing import Tuple, List, Callable, Optional
+from typing import Tuple, List, Callable, Optional, Dict
 
 Vertex = str
 Cost = float
-Edge = Tuple[Vertex, Vertex, Cost]
-Graph = Tuple[List[Vertex], List[Edge]]
-Solution = List[Edge]
+CostFunction = Dict[Vertex, Dict[Vertex, Cost]]
+Graph = Tuple[List[Vertex], CostFunction]
+Solution = List[Vertex]
 Solver = Callable[[Graph], Solution]
 Optimizer = Callable[[Solver], Solver]
 Evaluation = Tuple[Solver, Optional[Optimizer], Graph, Solution, Cost]
--- Dissertation/notebooks/survey_of_discrete_algorithms/num_evaluations.sh
+++ Dissertation/notebooks/survey_of_discrete_algorithms/num_evaluations.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while true; do
-	n=$(cat evaluations/*.dat | grep -c "N")
+	n=$(wc -l evaluations/*.dat | grep total)
 	now=$(date +"%T")
-	echo -ne "\033[1K\r$now   $n"
+	echo -ne "\033[1K\r$now$n"
 	sleep 1
 done
